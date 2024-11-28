e3cabf7dd19558e8ad0c7e397ed3d6838bb13a6e
Kiiiiiikikiiiki <ginocad.dev@gmail.com>
2024-11-28 14:47:44 -0500

    class employe et class garderie BEAN

--- src/com/littlenakamas/bean/Employe.java
+++ src/com/littlenakamas/bean/Employe.java
@@ -1,4 +1,13 @@
 package com.littlenakamas.bean;
 
 public class Employe {
+    public int numEmp;
+    public String nomEmp;
+    public String preEmp;
+
+    public Employe(int numEmp, String nomEmp, String preEmp) {
+        this.numEmp = numEmp;
+        this.nomEmp = nomEmp;
+        this.preEmp = preEmp;
+    }
 }
