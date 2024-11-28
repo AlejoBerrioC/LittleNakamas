62ef7c52a867f4154ffd42f696f1f293f13cc4ef
Kiiiiiikikiiiki <ginocad.dev@gmail.com>
2024-11-28 15:12:52 -0500

    finishing class enfant et parent

--- src/com/littlenakamas/bean/Enfant.java
+++ src/com/littlenakamas/bean/Enfant.java
@@ -1,5 +1,40 @@
 package com.littlenakamas.bean;
 
-public class Enfant {
+public class Enfant implements Affectation{
     public int numEnf;
+    public String nomEnf;
+    public String prenEnf;
+    public int ageEnf;
+    private Parent parentEnf;
+
+    public Enfant(int numEnf, String nomEnf, String prenEnf, int ageEnf, Parent parentEnf) {
+        this.numEnf = numEnf;
+        this.nomEnf = nomEnf;
+        this.prenEnf = prenEnf;
+        this.ageEnf = (ageEnf > 0) ? ageEnf : 0;
+        this.parentEnf = parentEnf;
+    }
+
+    public Parent getParentEnf() {
+        return parentEnf;
+    }
+
+    public void setParentEnf(Parent parentEnf) {
+        this.parentEnf = parentEnf;
+    }
+
+    @Override
+    public String affectedSection(int age) {
+        if (age <= 2){
+            return "";
+        }
+        else if (age <= 4) {
+            return "";
+        }
+        else if (age <= 6) {
+            return "";
+        } else {
+            return "";
+        }
+    }
 }
--- src/com/littlenakamas/bean/Parent.java
+++ src/com/littlenakamas/bean/Parent.java
@@ -1,4 +1,13 @@
 package com.littlenakamas.bean;
 
 public class Parent {
+    public String numTel;
+    public String nomPar;
+    public String prePar;
+
+    public Parent(String numTel, String nomPar, String prePar) {
+        this.numTel = numTel;
+        this.nomPar = nomPar;
+        this.prePar = prePar;
+    }
 }
