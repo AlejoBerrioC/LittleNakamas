a866a3aba8daf54b1856a0e23b953a0dc694297d
Kiiiiiikikiiiki <ginocad.dev@gmail.com>
2024-11-30 13:08:26 -0500

    Servlet teachers, admins, employees

--- src/com/littlenakamas/servlet/AdminsServlet.java
+++ src/com/littlenakamas/servlet/AdminsServlet.java
@@ -1,4 +1,19 @@
 package com.littlenakamas.servlet;
 
-public class AdminsServlet {
+import com.littlenakamas.dao.EmployeDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/admins")
+public class AdminsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("admins", new EmployeDAO().getAdminis());
+        this.getServletContext().getRequestDispatcher("/admins.jsp").forward(req, resp);
+    }
 }
--- src/com/littlenakamas/servlet/TeachersServlet.java
+++ src/com/littlenakamas/servlet/TeachersServlet.java
@@ -1,4 +1,19 @@
 package com.littlenakamas.servlet;
 
-public class TeachersServlet {
+import com.littlenakamas.dao.EmployeDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/teachers")
+public class TeachersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("teachers", new EmployeDAO().getTeachers());
+        this.getServletContext().getRequestDispatcher("/teachers.jsp").forward(req, resp);
+    }
 }
