be00b3a9fbb7f843ecbfd20d21af8dd11d763d9c
Kiiiiiikikiiiki <ginocad.dev@gmail.com>
2024-11-30 10:40:05 -0500

    EnfantsServlet doGet

---
+++ src/com/littlenakamas/servlet/EnfantsServlet.java
@@ -0,0 +1,30 @@
+package com.littlenakamas.servlet;
+
+import com.littlenakamas.bean.Enfant;
+import com.littlenakamas.bean.Inscription;
+import com.littlenakamas.dao.EnfantDAO;
+import com.littlenakamas.dao.InscriptionDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/enfant")
+public class EnfantsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ArrayList<Enfant> enfants = new EnfantDAO().getChildrens();
+        ArrayList<Inscription> inscriptions = new ArrayList<>();
+        for (Enfant enfant : enfants){
+            inscriptions.add(new InscriptionDAO().getInscriptionByNumEnf(enfant.numEnf));
+        }
+        req.setAttribute("enfants", new EnfantDAO().getChildrens());
+        req.setAttribute("dates", inscriptions);
+
+        this.getServletContext().getRequestDispatcher("/childrens.jsp").forward(req, resp);
+    }
+}
