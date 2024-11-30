d5b617e042426974b8f4b550e9b9f67689568a5c
Kiiiiiikikiiiki <ginocad.dev@gmail.com>
2024-11-30 13:40:35 -0500

    Servlet ParentsServlet POST

--- src/com/littlenakamas/servlet/ParentsServlet.java
+++ src/com/littlenakamas/servlet/ParentsServlet.java
@@ -1,5 +1,6 @@
 package com.littlenakamas.servlet;
 
+import com.littlenakamas.bean.Parent;
 import com.littlenakamas.dao.ParentDAO;
 
 import javax.servlet.ServletException;
@@ -15,5 +16,14 @@ public class ParentsServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setAttribute("parents", new ParentDAO().getParents());
         this.getServletContext().getRequestDispatcher("/parents.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("parent-name");
+        String lastname = req.getParameter("parent-lastname");
+        String number = req.getParameter("parent-number");
+
+        new ParentDAO().addParent(new Parent(number, lastname, name));
     }
 }
