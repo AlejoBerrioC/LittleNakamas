2d5407b093ed153e9dad5b02686dda79001b9032
Kiiiiiikikiiiki <ginocad.dev@gmail.com>
2024-12-02 22:52:13 -0500

    typo that caused error in code woups

--- src/com/littlenakamas/servlet/ParentsServlet.java
+++ src/com/littlenakamas/servlet/ParentsServlet.java
@@ -38,7 +38,6 @@ public class ParentsServlet extends HttpServlet {
                 String name = req.getParameter("parent-name");
                 String lastname = req.getParameter("parent-lastname");
                 String number = req.getParameter("parent-number");
-ç
                 new ParentDAO().addParent(new Parent(number, lastname, name));
 
                 resp.sendRedirect("enfant");
